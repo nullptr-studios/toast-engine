85392a3cce851ba1607cbdaeb94f46216a01af55
Iñaki <160393762+Omagoge@users.noreply.github.com>
2026-01-19 10:39:49 +0100

    epsilon fix

--- src/Physics/Raycast.cpp
+++ src/Physics/Raycast.cpp
@@ -12,7 +12,7 @@ std::optional<RayResult> RayCast(const vec2 point, const vec2 dir) {
 	const vec2 normal = { -direction.y, direction.x };
 	Line ray = {
 		.p1 = point,
-		.p2 = point + normalize(dir) * std::numeric_limits<float>::epsilon(),
+		.p2 = point + normalize(dir) * std::numeric_limits<float>::max(),
 		.normal = normal,
 		.tangent = direction
 	};
