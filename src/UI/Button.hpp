f897b0d866f262a68d817419f03bae429f20598b
Dante Harper <dantedigdog@gmail.com>
2026-01-19 10:59:36 +0100

    dante_ui init

---
+++ src/UI/Button.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file Button.hpp
+ * @author Dante Harper
+ * @date 19/01/26
+ */
+
+#pragma once
+
+#include "Toast/Objects/Actor.hpp"
+
+class Button : public toast::Actor {
+
+  REGISTER_TYPE(Button);
+
+  void Init() override {}
+
+};
