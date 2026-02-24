8feae46eb3d1e834b4dca1ce02f9b34029ded8fa
Darío Rodríguez Martínez <dariormt@gmail.com>
2026-02-24 02:13:46 +0100

    amongus

--- inc/Toast/Components/SpineRendererComponent.hpp
+++ inc/Toast/Components/SpineRendererComponent.hpp
@@ -46,6 +46,8 @@ public:
 			m_skeleton->updateWorldTransform(spine::Physics_None);
 		}
 	}
+	
+	spine::AnimationStateData* GetSkeletonData() const { return m_animationStateData.get(); }
 
 	void Load(json_t j, bool force_create = true) override;
 	json_t Save() const override;
