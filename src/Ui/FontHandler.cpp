1057c299ac6ced27190c4faafa47f36d0928d320
Dante Harper <dantedigdog@gmail.com>
2026-02-25 20:49:47 +0100

    refactor - dario is as black as claude ai

---
+++ src/Ui/FontHandler.cpp
@@ -0,0 +1,76 @@
+#include "AppCore/Platform.h"
+#include "Toast/Log.hpp"
+#include "Toast/Resources/ResourceManager.hpp"
+
+#include <Toast/Ui/FontHandler.hpp>
+#include <Ultralight/platform/FontLoader.h>
+
+using namespace ultralight;
+
+namespace ui {
+
+auto UiFontLoader::get() -> UiFontLoader& {
+	static UiFontLoader instance;
+	return instance;
+}
+
+void UiFontLoader::DestroyBuffer(void* user_data [[maybe_unused]], void* data) {
+	// delete[] static_cast<char*>(data);
+}
+
+[[nodiscard]]
+String UiFontLoader::fallback_font() const {
+	return "Monocraft";
+}
+
+[[nodiscard]]
+String UiFontLoader::fallback_font_for_characters(const String& characters, int weight, bool italic) const {    // NOLINT
+	                                                                                                              //
+	TOAST_TRACE("[FontLoader] fallback_font_for_characters() called, weight={} italic={}", weight, italic);
+	TOAST_ERROR("TELL DANTE IF THE ACTUALLY HAPPENS OR NOT");
+	return fallback_font();
+}
+
+RefPtr<FontFile> UiFontLoader::Load(const String& family, int weight, bool italic) {
+	TOAST_TRACE("[FontLoader] Loading font: '{}' weight={} italic={}", family.utf8().data(), weight, italic);
+	// Map weight values to font names
+	std::string weight_name;
+	if (weight >= 900) {
+		weight_name = "-Black";
+	} else if (weight >= 700) {
+		weight_name = "-Bold";
+	} else if (weight >= 600) {
+		weight_name = "-SemiBold";
+	} else if (weight >= 500) {
+		weight_name = "-Medium";
+	} else if (weight < 300) {
+		weight_name = "-ExtraLight";
+	} else if (weight < 400) {
+		weight_name = "-Light";
+	} else {
+		weight_name = "-Regular";
+	}
+
+	// Build filename
+	std::string filename = "FONTS/";
+	filename.append(family.utf8().data()).append("/").append(family.utf8().data());
+	if (!weight_name.empty()) {
+		filename += weight_name;
+	}
+
+	if (italic) {
+		filename += "-Italic";
+	}
+	filename += ".ttf";
+
+	auto file = resource::Open(filename);
+	if (file.has_value()) {
+		TOAST_TRACE("[Font Loader] Loading Font ... {}", filename);
+		// auto buffer = Buffer::Create(file->data(), file->size(), nullptr, &UiFontLoader::DestroyBuffer);
+		return FontFile::Create(filename.data());
+	}
+	TOAST_WARN("Font Not Found: {} falling back onto platform", filename);
+	auto* font_loader = ultralight::GetPlatformFontLoader();
+	return font_loader->Load(family, weight, italic);
+}
+}
--- src/Ui/Logger.cpp
+++ src/Ui/Logger.cpp
@@ -1,12 +1,13 @@
 #include <Toast/Log.hpp>
-#include <Toast/Renderer/HUD/ToastLogger.hpp>
+#include <Toast/Ui/Logger.hpp>
 
-ToastLogger& ToastLogger::Get() {
-	static ToastLogger instance;
+namespace ui {
+auto UiLogger::get() -> UiLogger& {
+	static UiLogger instance;
 	return instance;
 }
 
-void ToastLogger::LogMessage(ultralight::LogLevel log_level, const ultralight::String& message) {
+void UiLogger::LogMessage(ultralight::LogLevel log_level, const ultralight::String& message) {
 	std::string ul_msg = message.utf8().data();
 	switch (log_level) {
 		case ultralight::LogLevel::Error: TOAST_ERROR("[Ultralight] {}", ul_msg); break;
@@ -14,4 +15,5 @@ void ToastLogger::LogMessage(ultralight::LogLevel log_level, const ultralight::S
 		case ultralight::LogLevel::Info:
 		default: TOAST_TRACE("[Ultralight] {}", ul_msg); break;
 	}
+}
 }
