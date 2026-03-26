550dbaf076df0bdf6a7f38dbf87c0128a38938d3
Darío Rodríguez Martínez <dariormt@gmail.com>
2026-03-26 15:59:13 +0100

    some stuff

--- inc/Toast/Renderer/IPostProcess.hpp
+++ inc/Toast/Renderer/IPostProcess.hpp
@@ -5,11 +5,15 @@
 #pragma once
 
 #include "Framebuffer.hpp"
+#include "Toast/ISerializable.hpp"
 
 struct IPostProcess {
 	virtual ~IPostProcess() = default;
 
 	virtual void Execute(Framebuffer* inputFBO, Framebuffer* outputFBO) = 0;
+	
+	virtual json_t Save() = 0;
+	virtual void Load(json_t&) = 0;
 
 #ifdef TOAST_EDITOR
 	virtual void Inspector() = 0;
