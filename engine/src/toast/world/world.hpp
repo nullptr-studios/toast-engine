e6f72231a31343039d6f256f56f39df4cb163030
Xein <xgonip@gmail.com>
2026-05-21 09:07:28 +0200

    Tarjan SCCs and Wave assignment algorithms

--- engine/src/toast/world/world.hpp
+++ engine/src/toast/world/world.hpp
@@ -7,6 +7,7 @@
  */
 
 #pragma once
+#include "../log.hpp"
 #include "box.hpp"
 #include "control_box.hpp"
 #include "node.hpp"
@@ -61,6 +62,10 @@ public:
 	 * @param to Node that will be ticked last
 	 */
 	static auto registerDependency(Node& from, Node& to) {
+		if (&from == &to) {
+			TOAST_WARN("World", "{} ({}) tried to register a dependency to itself", from.name(), from.uuid());
+			return;
+		}
 		// TODO: sanity check
 		// auto& node_set = instance->m.nodes;
 		// if (node_set.find(from.box()) == node_set.end()) {
@@ -158,6 +163,178 @@ public:
 		// Phase 3: Tarjan algorithm and cluster nodes creation
 		// We are going to detect SCCs using the Tarjan algorithm and then create ClusterNodes from those SCCs so they
 		// are ticked synchronously and don't result in any race condition
+		struct TarjanContext {
+			std::unordered_map<Box<Node>, int> index;
+			std::unordered_map<Box<Node>, int> low_link;
+			std::unordered_map<Box<Node>, bool> on_stack;
+			std::stack<Box<Node>> stack;
+			int counter = 0;
+			std::vector<std::vector<Box<Node>>> SCCs; // reverse topology order
+		};
+
+		std::vector<std::vector<std::variant<Box<Node>, NodeCluster>>> result_subgraphs;
+
+		for (auto& g : subgraphs) {
+			// Build surviving nodes for edge filtering
+			std::unordered_set<Box<Node>> in_subgraph;
+			for (auto& v : g) in_subgraph.insert(std::get<Box<Node>>(v));
+
+			TarjanContext ctx;
+			std::function<void(const Box<Node>&)> strong_connect = [&](const Box<Node>& v) {
+				ctx.index[v] = ctx.low_link[v] = ctx.counter++;
+				ctx.stack.push(v);
+				ctx.on_stack[v] = true;
+
+				if (auto it = graph.connections.find(v); it != graph.connections.end()) {
+					for (const auto& w : it->second) {
+						if (!in_subgraph.count(w)) {
+							// Skip nodes pruned in phase 2
+							continue;
+						}
+
+						if (!ctx.index.count(w)) {
+							strong_connect(w);
+							ctx.low_link[v] = std::min(ctx.low_link[v], ctx.low_link[w]);
+						} else if (ctx.on_stack[w]) {
+							ctx.low_link[v] = std::min(ctx.low_link[v], ctx.low_link[w]);
+						}
+					}
+				}
+
+				// Root of an SCC: we pop it
+				if (ctx.low_link[v] == ctx.index[v]) {
+					std::vector<Box<Node>> scc;
+					while (true) {
+						auto w = ctx.stack.top();
+						ctx.stack.pop();
+						ctx.on_stack[w] = false;
+						scc.push_back(w);
+						if (w == v) break;
+					}
+					ctx.SCCs.emplace_back(std::move(scc));
+				}
+			};
+
+			for (auto& variant : g) {
+				const auto& node = std::get<Box<Node>>(variant);
+				if (!ctx.index.count(node)) {
+					strong_connect(node);
+				}
+			}
+
+			// SCCs is in reverse order so
+			std::vector<std::variant<Box<Node>, NodeCluster>> sorted_g;
+			sorted_g.reserve(ctx.SCCs.size());
+
+			for (auto& scc : ctx.SCCs) {
+				if (scc.size() == 1) {
+					sorted_g.emplace_back(scc[0]);
+				} else {
+					TOAST_TRACE("World", "Dependency cycle detected ({} nodes) -> grouping into NodeCluster", scc.size());
+					sorted_g.emplaceBack(NodeCluster(scc));
+				}
+			}
+			result_subraphs.emplace_back(std::move(sorted_g));
+		}
+
+		subgraphs = std::move(result_subgraphs);
+
+		// Phase 4: Wave algorithm
+		// Assigns a wave number to each item
+		// Items sharing a wave have no dependency on each other and can be ticked in parallel via futures
+
+		// Map every node back to its containing item so NodeClusters are treated as a single scheduling unit
+		struct ItemRef {
+			int subgraph;
+			int index;
+		};
+
+		std::unordered_map<Box<Node>, ItemRef> node_to_item;
+
+		for (int si = 0; si < (int)subgraphs.size(); ++si) {
+			for (int ii = 0; ii < (int)subgraphs[si].size(); ++ii) {
+				std::visit(
+				    [&](auto& item) {
+					    using T = std::decay_t<decltype(item)>;
+					    if constexpr (std::is_same_v<T, Box<Node>>) {
+						    node_to_item[item] = {si, ii};
+					    } else {    // NodeCluster
+						    for (const auto& n : item.nodes) {
+							    node_to_item[n] = {si, ii};
+						    }
+					    }
+				    },
+				    subgraphs[si][ii]
+				);
+			}
+		}
+
+		// Assign wave levels
+		std::vector<std::vector<int>> waves(subgraphs.size());
+		for (int si = 0; si < (int)subgraphs.size(); ++si) {
+			waves[si].assign(subgraphs[si].size(), 0);
+		}
+
+		for (int si = 0; si < (int)subgraphs.size(); ++si) {
+			// Reverse iteration vecause of tarjan
+			for (int ii = (int)subgraphs[si].size() - 1; ii >= 0; --ii) {
+				// Collect all physical nodes belonging to this item
+				std::vector<Box<Node>> item_nodes;
+				std::visit(
+				    [&](auto& item) {
+					    using T = std::decay_t<decltype(item)>;
+					    if constexpr (std::is_same_v<T, Box<Node>>) {
+						    item_nodes.push_back(item);
+					    } else {
+						    item_nodes.insert(item_nodes.end(), item.nodes.begin(), item.nodes.end());
+					    }
+				    },
+				    subgraphs[si][ii]
+				);
+
+				int max_pred_wave = -1;
+				for (const auto& node : item_nodes) {
+					auto it = graph.inverse_connections.find(node);
+					if (it == graph.inverse_connections.end()) {
+						continue;
+					}
+
+					for (const auto& pred : it->second) {
+						auto pit = node_to_item.find(pred);
+						if (pit == node_to_item.end()) {
+							continue;    // pruned in Phase 2
+						}
+
+						auto [psi, pii] = pit->second;
+						if (psi == si && pii != ii) {    // skip intra-cluster self-edges
+							max_pred_wave = std::max(max_pred_wave, waves[psi][pii]);
+						}
+					}
+				}
+				waves[si][ii] = max_pred_wave + 1;
+			}
+		}
+
+		// Find the highest wave across all subgraphs
+		int max_wave = 0;
+		for (const auto& sg : waves) {
+			for (int w : sg) {
+				max_wave = std::max(max_wave, w);
+			}
+		}
+
+		// Bucket items by wave level
+		// Each bucket is a parallel dispatch barrier: all items within a bucket can run concurrently
+		// The next bucket will then only start when all futures join
+		std::vector<std::vector<std::variant<Box<Node>, NodeCluster>>> tick_schedule(max_wave + 1);
+
+		for (int si = 0; si < (int)subgraphs.size(); ++si) {
+			for (int ii = 0; ii < (int)subgraphs[si].size(); ++ii) {
+				tick_schedule[waves[si][ii]].emplace_back(std::move(subgraphs[si][ii]));
+			}
+		}
+
+		m.tick_schedule = std::move(tick_schedule);
 	}
 
 	void swapRoot(Node* node);
@@ -181,6 +358,7 @@ private:
 		event::Listener listener;
 
 		std::unordered_set<_detail::ControlBox> nodes;
+		std::vector<std::vector<std::variant<Box<Node>, NodeCluster>>> tick_schedule;
 
 		Box<Node> root_node;
 		std::vector<Box<Node>> global_nodes;
