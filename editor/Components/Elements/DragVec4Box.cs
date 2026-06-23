0c2d8efc48a7ca0048759f1ba2519f28d6443df7
Xein <xgonip@gmail.com>
2026-06-24 00:47:53 +0200

    vec2 vec3 vec4

---
+++ editor/Components/Elements/DragVec4Box.cs
@@ -0,0 +1,84 @@
+//
+// DragVec4Box.cs by Xein
+// 23 Jun 2026
+//
+
+using Avalonia;
+using Avalonia.Data;
+
+namespace editor.Components.Elements;
+
+public sealed class DragVec4Box : DragVectorBoxBase {
+	public static readonly StyledProperty<float> XProperty =
+		AvaloniaProperty.Register<DragVec4Box, float>(nameof(X), defaultBindingMode: BindingMode.TwoWay);
+
+	public static readonly StyledProperty<float> YProperty =
+		AvaloniaProperty.Register<DragVec4Box, float>(nameof(Y), defaultBindingMode: BindingMode.TwoWay);
+
+	public static readonly StyledProperty<float> ZProperty =
+		AvaloniaProperty.Register<DragVec4Box, float>(nameof(Z), defaultBindingMode: BindingMode.TwoWay);
+
+	public static readonly StyledProperty<float> WProperty =
+		AvaloniaProperty.Register<DragVec4Box, float>(nameof(W), defaultBindingMode: BindingMode.TwoWay);
+
+	public static readonly StyledProperty<string> LabelXProperty =
+		AvaloniaProperty.Register<DragVec4Box, string>(nameof(LabelX), "X");
+
+	public static readonly StyledProperty<string> LabelYProperty =
+		AvaloniaProperty.Register<DragVec4Box, string>(nameof(LabelY), "Y");
+
+	public static readonly StyledProperty<string> LabelZProperty =
+		AvaloniaProperty.Register<DragVec4Box, string>(nameof(LabelZ), "Z");
+
+	public static readonly StyledProperty<string> LabelWProperty =
+		AvaloniaProperty.Register<DragVec4Box, string>(nameof(LabelW), "W");
+
+	public float X {
+		get => GetValue(XProperty);
+		set => SetValue(XProperty, value);
+	}
+
+	public float Y {
+		get => GetValue(YProperty);
+		set => SetValue(YProperty, value);
+	}
+
+	public float Z {
+		get => GetValue(ZProperty);
+		set => SetValue(ZProperty, value);
+	}
+
+	public float W {
+		get => GetValue(WProperty);
+		set => SetValue(WProperty, value);
+	}
+
+	public string LabelX {
+		get => GetValue(LabelXProperty);
+		set => SetValue(LabelXProperty, value);
+	}
+
+	public string LabelY {
+		get => GetValue(LabelYProperty);
+		set => SetValue(LabelYProperty, value);
+	}
+
+	public string LabelZ {
+		get => GetValue(LabelZProperty);
+		set => SetValue(LabelZProperty, value);
+	}
+
+	public string LabelW {
+		get => GetValue(LabelWProperty);
+		set => SetValue(LabelWProperty, value);
+	}
+
+	protected override int AxisCount => 4;
+
+	protected override AxisSpec[] BuildAxes() => [
+		new AxisSpec(LabelXProperty, "Red", XProperty),
+		new AxisSpec(LabelYProperty, "Green", YProperty),
+		new AxisSpec(LabelZProperty, "Blue", ZProperty),
+		new AxisSpec(LabelWProperty, "Magenta", WProperty)
+	];
+}
--- editor/Workspace/InspectorViewModel.cs
+++ editor/Workspace/InspectorViewModel.cs
@@ -7,4 +7,16 @@ public partial class InspectorViewModel : Tool {
 	[ObservableProperty] private float m_posX = -12.5f;
 	[ObservableProperty] private float m_mass = 3.25f;
 	[ObservableProperty] private int m_count = 7;
+
+	[ObservableProperty] private float m_vec2X = 1.0f;
+	[ObservableProperty] private float m_vec2Y = 2.0f;
+
+	[ObservableProperty] private float m_vec3X = 1.0f;
+	[ObservableProperty] private float m_vec3Y = 2.0f;
+	[ObservableProperty] private float m_vec3Z = 3.0f;
+
+	[ObservableProperty] private float m_vec4X;
+	[ObservableProperty] private float m_vec4Y = 0.5f;
+	[ObservableProperty] private float m_vec4Z = 0.75f;
+	[ObservableProperty] private float m_vec4W = 1.0f;
 }
